718b6391423acb9a06f80461053ad6b52d477259
Chandra, D <csreddy1998@gmail.com>
2025-05-30 10:30:38 -0500

    feature: Implement articles resource with CRUD operations and validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,38 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
+  def index
+    @articles = Article.ordered_by_published_date
+    render json: @articles
+  end
+
+  def show
+    render json: @article
+  end
+
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      render json: @article, status: :created
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  def method_not_allowed
+    head :method_not_allowed
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  end
+
+  def article_params
+    params.permit(:title, :content, :author, :category, :published_at)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,19 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :content, presence: true
+  validates :author, presence: true
+  validates :published_at, presence: true
+
+  scope :ordered_by_published_date, -> { order(published_at: :desc) }
+
+  def as_json(options = {})
+    {
+      id: id,
+      title: title,
+      content: content,
+      author: author,
+      category: category,
+      published_at: published_at.strftime('%Y-%m-%d')
+    }
+  end
 end
