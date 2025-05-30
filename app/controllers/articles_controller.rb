754828a4c90d85437941a5b1da3fdc4734d83942
Chandra, D <csreddy1998@gmail.com>
2025-05-30 11:44:09 -0500

    docs: Add comments to articles controller actions for clarity

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
 
+  # GET /articles -- List all articles ordered by published date
   def index
     @articles = Article.ordered_by_published_date
     render json: @articles
   end
 
+  # GET /articles/:id -- Show a specific article
   def show
     render json: @article
   end
 
+  # POST /articles -- Create a new article
   def create
     @article = Article.new(article_params)
     
@@ -20,6 +23,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # DELETE, PUT, PATCH methods are not allowed 
   def method_not_allowed
     head :method_not_allowed
   end
